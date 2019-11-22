1687e3bf8420e211100bb937f2f4b6bac7d863e3
John Xu <zhxu73@email.arizona.edu>
2019-11-22 05:02:17 -0700

    cache is default to be enabled in config

--- lib/sequenceserver/config.rb
+++ lib/sequenceserver/config.rb
@@ -106,7 +106,8 @@ module SequenceServer
           tblastx: ['-evalue 1e-5'],
           tblastn: ['-evalue 1e-5']
         },
-        num_threads: 1
+        num_threads: 1,
+        cache: "enable"
       }
     end
   end
